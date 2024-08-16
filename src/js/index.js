92a8163972e80095c6dac54d8ce118fd87745f23
luh17008 <luizadeoliveirasilva750@gmail.com>
2024-08-16 10:36:50 -0300

    projeto finalizado

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+function calcularIMC() {
+    const altura = parseFloat(document.getElementById('altura').value);
+    const peso = parseFloat(document.getElementById('peso').value);
+
+    if (isNaN(altura) || isNaN(peso)) {
+        alert('Por favor, insira valores válidos para altura e peso.');
+        return;
+    }
+
+    const imc = peso / (altura * altura);
+    let classificacao = '';
+
+    if (imc < 18.5) {
+        classificacao = 'Abaixo do peso';
+    } else if (imc >= 18.5 && imc < 24.9) {
+        classificacao = 'Peso normal';
+    } else if (imc >= 25 && imc < 29.9) {
+        classificacao = 'Sobrepeso';
+    } else {
+        classificacao = 'Obesidade';
+    }
+
+    document.getElementById('resultado').innerText = `Seu IMC é ${imc.toFixed(2)} (${classificacao})`;
+}
